8bd5a6b7f3c4fd0de5ecfc96f9ca96e3d2e3add2
Sulthan Farizan Fawwaz <farizanfawwaz54@gmail.com>
2025-11-10 01:04:12 +0800

    progress mc

--- src/data/menu.js
+++ src/data/menu.js
@@ -3,35 +3,35 @@ export const appertizerMenu = [
         id: 'appertizer1',
         name: 'Baba Gnaoush',
         description: 'Dip terong panggang lembut dengan tahini, bawang putih, lemon, dan minyak zaitun.',
-        image: './src/images/menu/appertizer-dessert/baba-gnaoush.jpg',
+        image: '@/src/images/menu/appertizer-dessert/baba-gnaoush.jpg',
         price: 'RP 50.000',
     },
     {
         id: 'appertizer2',
         name: 'Samosa',
         description: 'Kulit tipis goreng berisi daging berbumbu dan sayuran, disajikan hangat dengan saus yoghurt mint.',
-        image: './src/images/menu/appertizer-dessert/samosa.jpeg',
+        image: '@/src/images/menu/appertizer-dessert/samosa.jpeg',
         price: 'RP 40.000',
     },
     {
         id: 'appertizer3',
         name: 'Risoles',
         description: 'Tersedia 2 rasa : isi telur + Bihun dan telur + sosis',
-        image: './src/images/menu/appertizer-dessert/risol.jpg',
+        image: '@/src/images/menu/appertizer-dessert/risol.jpg',
         price: 'RP 20.000',
     },
     {
         id: 'appertizer4',
         name: 'Hummus',
         description: 'Dip lembut dari kacang arab tumbuk dengan tahini, bawang putih, lemon, dan minyak zaitun.',
-        image: './src/images/menu/appertizer-dessert/hummus.jpeg',
+        image: '@/src/images/menu/appertizer-dessert/hummus.jpeg',
         price: 'RP 40.000',
     },
     {
         id: 'appertizer5',
         name: 'Falafel',
         description: 'Bola goreng renyah dari kacang arab dan rempah khas Timur Tengah, disajikan dengan saus tahini.',
-        image: './src/images/menu/falafel.jpeg',
+        image: '@/src/images/menu/falafel.jpeg',
         price: 'RP 35.000',
     },
 ]
@@ -46,28 +46,46 @@ export const mainCourseMenu = [
                 id: 'mc1',
                 name: 'Nasi Briyani Daging Domba',
                 price: 'RP 70.000',
-                image: './src/images/menu/maincourse-sidedish/briyani domba.png'
+                image: '@/src/images/menu/maincourse-sidedish/briyani domba.png'
             },
             {
                 id: 'mc2',
                 name: 'Nasi Briyani Daging Ayam',
                 price: 'RP 55.000',
-                image: './src/images/menu/maincourse-sidedish/briyani ayam.jpg'
+                image: '@/src/images/menu/maincourse-sidedish/briyani ayam.jpg'
             },
             {
                 id: 'mc3',
                 name: 'Nasi Briyani Daging Sapi',
                 price: 'RP 70.000',
-                image: './src/images/menu/maincourse-sidedish/briyani sapi.png'
+                image: '@/src/images/menu/maincourse-sidedish/briyani sapi.png'
             },
         ]
     },
     {
         groupId: 'kebuli',
         groupName: 'B. Nasi Kebuli',
-        groupDesc: 'Nasi gurih berbumbu rempah dan kaldu daging, disajikan dengan kambing atau ayam serta acar dan sambal.'
+        groupDesc: 'Nasi gurih berbumbu rempah dan kaldu daging, disajikan dengan kambing atau ayam serta acar dan sambal.',
         items: [
-
+            {
+                id: 'mc4',
+                name: 'Nasi Kebuli Daging Domba',
+                price: 'RP 70.000',
+                image: '@/src/images/menu/maincourse-sidedish/kebuli domba.png'
+            },
+            {
+                id: 'mc5',
+                name: 'Nasi Kebuli Daging Ayam',
+                price: 'RP 55.000',
+                image: '@/src/images/menu/maincourse-sidedish/kebuli ayam.jpg'
+            },
+            {
+                id: 'mc6',
+                name: 'Nasi Kebuli Daging Sapi',
+                price: 'RP 70.000',
+                image: '@/src/images/menu/maincourse-sidedish/kebuli sapi.png'
+            },
         ]
     },
+    {},
 ]
