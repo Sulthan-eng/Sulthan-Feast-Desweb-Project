e8ae3d27889c6c18c4ce52ba4f4b6c72391e4fd3
Sulthan Farizan Fawwaz <farizanfawwaz54@gmail.com>
2025-11-11 00:13:30 +0800

    adding a new file and test footer navbar in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,18 @@
-<script setup></script>
+<script setup>
+import Navbar from "@/components/layout/Navbar.vue";
+import Footer from "@/components/layout/Footer.vue";
+import {RouterView} from "vue-router"; //nampilkan halaman
+</script>
 
 <template>
+  <Navbar />
+
+  <main>
+    <RouterView />
+  </main>
+
+  <Footer />
+
 <!--  <nav class="bg-black  p-5 text-white flex justify-center space-x-5">-->
 <!--    <router-link to="/" class="hover:text-yellow-300">Home</router-link>-->
 <!--    <router-link to="/menu" class="hover:text-yellow-300">Menu</router-link>-->
