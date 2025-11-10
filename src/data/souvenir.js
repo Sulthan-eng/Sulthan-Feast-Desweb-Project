8a96e2a3c2761e1ef821da48164880a3850fb22d
Sulthan Farizan Fawwaz <farizanfawwaz54@gmail.com>
2025-11-10 21:13:34 +0800

    fix souvenir.js

---
+++ src/data/souvenir.js
@@ -0,0 +1,32 @@
+//SNACKS
+export const snacks = [
+    {
+        id: 'snack1',
+        name: 'Kurma',
+        image: '@/src/assets/images/souvenirs&gifts/kurma.jpg',
+    },
+    {
+        id: 'snack2',
+        name: 'Kismis',
+        image: '@/src/assets/images/souvenirs&gifts/kismis.jpeg'
+    },
+    {
+        id: 'snack3',
+        name: 'Kacang Arab',
+        image: '@/src/assets/images/souvenirs&gifts/kacang arab.jpeg'
+    }
+];
+
+//SOUVENIRS & CRAFTS
+export const crafts = [
+    {
+        id: 'craft1',
+        name: 'Tasbih',
+        image: '@/src/assets/images/souvenirs&gifts/tasbih.jpeg'
+    },
+    {
+        id: 'craft2',
+        name: 'Gantungan Kunci',
+        image: '@/src/assets/images/souvenirs&gifts/gantungan kunci.png'
+    }
+];
