b40deb9c2bb1b0e772464b8b29b03b664b0b11e3
Sulthan Farizan Fawwaz <farizanfawwaz54@gmail.com>
2025-11-10 23:02:46 +0800

    try to create navbar and footer structure

--- src/components/layout/Footer.vue
+++ src/components/layout/Footer.vue
@@ -3,7 +3,27 @@
 </script>
 
 <template>
+  <Footer>
+    <div>
+      <div>
+        <h3>Lokasi</h3>
+        <p>Jl. Ruhui Rahayu No.36</p>
+        <p>Gunung Bahagia, Balikpapan</p>
+        <p>Kalimantan Timur</p>
+      </div>
 
+      <div>
+        <h3>Kontak</h3>
+        <p>Whatsapp: 0853-9141-9495</p>
+        <p>Telepon: 0542-7341-0908</p>
+      </div>
+
+      <div>
+        <h3>Media Sosial</h3>
+        <p></p>
+      </div>
+    </div>
+  </Footer>
 </template>
 
 <style scoped>
