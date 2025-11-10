a8f36cb972be776a67e3fc3cbee3703143c593f6
Sulthan Farizan Fawwaz <farizanfawwaz54@gmail.com>
2025-11-10 20:56:59 +0800

    fix room.js

---
+++ src/data/room.js
@@ -0,0 +1,20 @@
+export const room = [
+    {
+        id: 'room1',
+        name: 'VIP Room 1,2,3',
+        description: 'Ruangan khusus untuk keluarga dan sahabat dengan nuansa khas timur tengah.',
+        image: '@/src/assets/images/room/vip room.jpeg',
+    },
+    {
+        id: 'room2',
+        name: 'VVIP Room',
+        description: 'Ruangan khusus untuk keluarga dan sahabat dengan nuansa khas timur tengah.',
+        image: '@/src/assets/images/room/vvip room.png',
+    },
+    {
+        id: 'room3',
+        name: 'Meeting Room',
+        description: 'Ruangan khusus untuk rapat.',
+        image: '@/src/assets/images/room/meeting room.png',
+    },
+]
