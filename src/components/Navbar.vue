3dc09d2e3ac262878d2217414a3c4b02fa9ce527
Sulthan Farizan Fawwaz <farizanfawwaz54@gmail.com>
2025-11-15 15:11:48 +0800

    finish Navbar and Footer, make a progress in Home page

---
+++ src/components/Navbar.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+import {RouterLink} from "vue-router";
+</script>
+
+<template>
+  <nav class="bg-black text-white fixed w-full z-20 top-0 start-0 border-b border-default">
+    <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-5">
+      <RouterLink to="/">
+        <img src="/Full_Logo_Sulthan_Feast.svg" class="h-18" alt="Sulthan Feast Logo" />
+      </RouterLink>
+
+<!--      hamburger-->
+      <button data-collapse-toggle="navbar-default" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-body rounded-base md:hidden hover:bg-neutral-secondary-soft hover:text-heading focus:outline-none focus:ring-2 focus:ring-neutral-tertiary" aria-controls="navbar-default" aria-expanded="false">
+        <span class="sr-only">Open main menu</span>
+        <svg class="w-6 h-6" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-width="2" d="M5 7h14M5 12h14M5 17h14"/></svg>
+      </button>
+
+
+      <div class="hidden w-full md:block md:w-auto" id="navbar-default">
+        <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-default rounded-base bg-neutral-secondary-soft md:flex-row md:space-x-12 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-neutral-primary">
+          <li>
+            <RouterLink to="/" active-class="text-yellow-400" class="block py-2 px-3 text-white rounded md:bg-transparent md:p-0 hover:text-yellow-300">Home</RouterLink>
+          </li>
+          <li>
+            <RouterLink to="/menu" active-class="text-yellow-400" class="block py-2 px-3 text-white rounded md:bg-transparent md:p-0 hover:text-yellow-300">Menu</RouterLink>
+          </li>
+          <li>
+            <RouterLink to="/room" active-class="text-yellow-400" class="block py-2 px-3 text-white rounded md:bg-transparent md:p-0 hover:text-yellow-300">Room</RouterLink>
+          </li>
+          <li>
+            <RouterLink to="/souvenirs"active-class="text-yellow-400" class="block py-2 px-3 text-white rounded md:bg-transparent md:p-0 hover:text-yellow-300">Souvenirs & Gifts</RouterLink>
+          </li>
+          <li>
+            <RouterLink to="/about" active-class="text-yellow-400" class="block py-2 px-3 text-white rounded md:bg-transparent md:p-0 hover:text-yellow-300">About</RouterLink>
+          </li>
+          <li>
+            <RouterLink to="/reservation" active-class="text-yellow-400" class="block py-2 px-3 text-white rounded md:bg-transparent md:p-0 hover:text-yellow-300">Reservation</RouterLink>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<style scoped>
+</style>
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
+import Hero from "@/components/Hero.vue";
 import Navbar from "@/components/Navbar.vue";
 </script>
 
 <template>
   <Navbar/>
+  <Hero/>
 </template>
 
 <style scoped>
