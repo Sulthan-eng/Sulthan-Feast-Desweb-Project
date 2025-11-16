5e635cf42c9d4d35199fac618a7a9a49f1722d1e
Sulthan Farizan Fawwaz <farizanfawwaz54@gmail.com>
2025-11-16 23:47:26 +0800

    new component import

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,11 +1,15 @@
 <script setup lang="ts">
+import BestSeller from "@/components/BestSeller.vue";
 import Hero from "@/components/Hero.vue";
 import Navbar from "@/components/Navbar.vue";
+import Footer from "@/components/Footer.vue";
 </script>
 
 <template>
   <Navbar/>
   <Hero/>
+  <BestSeller/>
+  <Footer/>
 </template>
 
 <style scoped>
