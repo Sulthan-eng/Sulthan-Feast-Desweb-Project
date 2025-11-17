8737ff6a122e6fd84fedf1bb91a1e1b77262cc1d
Sulthan Farizan Fawwaz <farizanfawwaz54@gmail.com>
2025-11-17 13:27:35 +0800

    +font awesome

--- src/main.js
+++ src/main.js
@@ -2,9 +2,16 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import './style.css'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faInstagram, faFacebook } from '@fortawesome/free-brands-svg-icons'
+
+library.add(faInstagram, faFacebook);
 
 const app = createApp(App)
 
 app.use(router)  //router digunakan
 
 app.mount('#app')
+
+app.component('font-awesome-icon', FontAwesomeIcon)
