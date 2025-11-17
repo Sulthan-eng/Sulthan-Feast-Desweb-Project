173d84a87448a7baff6b1ab0ed5dffba46d6fa47
Sulthan Farizan Fawwaz <farizanfawwaz54@gmail.com>
2025-11-17 21:13:01 +0800

    fix the format source image so that vite can read it correctly

--- src/data/room.js
+++ src/data/room.js
@@ -3,18 +3,18 @@ export const room = [
         id: 'room1',
         name: 'VIP Room 1,2,3',
         description: 'Ruangan khusus untuk keluarga dan sahabat dengan nuansa khas timur tengah.',
-        image: '@/src/assets/images/room/vip room.jpeg',
+        image: new URL('@/assets/images/room/vip_room.jpeg', import.meta.url).href
     },
     {
         id: 'room2',
         name: 'VVIP Room',
         description: 'Ruangan khusus untuk keluarga dan sahabat dengan nuansa khas timur tengah.',
-        image: '@/src/assets/images/room/vvip room.png',
+        image: new URL('@/assets/images/room/vvip_room.png', import.meta.url).href
     },
     {
         id: 'room3',
         name: 'Meeting Room',
         description: 'Ruangan khusus untuk rapat.',
-        image: '@/src/assets/images/room/meeting room.png',
+        image: new URL('@/assets/images/room/meeting_room.png', import.meta.url).href
     },
 ]
