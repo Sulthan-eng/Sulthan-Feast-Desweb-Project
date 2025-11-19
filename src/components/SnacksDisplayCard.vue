da005766883b6c6e1a48527ea00ce803c57da875
Sulthan Farizan Fawwaz <farizanfawwaz54@gmail.com>
2025-11-20 00:32:02 +0800

    a card  snack to reuse

---
+++ src/components/SnacksDisplayCard.vue
@@ -0,0 +1,16 @@
+<script setup>
+defineProps({
+    snacks: Object
+})
+
+</script>
+
+<template>
+    <div class="flex flex-col items-center p-4 bg-white rounded-lg shadow-mg">
+        <img :src="snacks.image" :alt="snacks.name" class="w-40 h-40 object-cover rounded-md"/>
+
+        <h3 class="mt-4 text-xl font-semibold text-black">
+            {{ snacks.name }}
+        </h3>
+    </div>
+</template>
