a7cc068278eaee3053b3d6917e525ca077bae541
Sulthan Farizan Fawwaz <farizanfawwaz54@gmail.com>
2025-11-20 00:30:53 +0800

    allahuakbar small mistakes

--- src/data/souvenir.js
+++ src/data/souvenir.js
@@ -3,17 +3,17 @@ export const snacks = [
     {
         id: 'snack1',
         name: 'Kurma',
-        image: new URL('@/src/assets/images/souvenirs&gifts/kurma.jpg', import.meta.url).href
+        image: new URL('@/assets/images/souvenirs-gifts/kurma.jpg', import.meta.url).href
     },
     {
         id: 'snack2',
         name: 'Kismis',
-        image: new URL('@/src/assets/images/souvenirs&gifts/kismis.jpeg',import.meta.url).href
+        image: new URL('@/assets/images/souvenirs-gifts/kismis.jpeg', import.meta.url).href
     },
     {
         id: 'snack3',
         name: 'Kacang Arab',
-        image: new URL('@/src/assets/images/souvenirs&gifts/kacang_arab.jpeg', import.meta.url).href
+        image: new URL('@/assets/images/souvenirs-gifts/kacang_arab.jpeg', import.meta.url).href
     }
 ];
 
@@ -22,11 +22,11 @@ export const crafts = [
     {
         id: 'craft1',
         name: 'Tasbih',
-        image: new URL('@/src/assets/images/souvenirs&gifts/tasbih.jpeg', import.meta.url).href
+        image: new URL('@/assets/images/souvenirs-gifts/tasbih.jpeg', import.meta.url).href
     },
     {
         id: 'craft2',
         name: 'Gantungan Kunci',
-        image: new URL('@/src/assets/images/souvenirs&gifts/gantungan kunci.png', import.meta.url).href
+        image: new URL('@/assets/images/souvenirs-gifts/gantungan_kunci.png', import.meta.url).href
     }
 ];
