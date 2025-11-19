2afbf0530d6ce825419dd27974f26253067e6deb
Sulthan Farizan Fawwaz <farizanfawwaz54@gmail.com>
2025-11-19 21:38:11 +0800

    delete unuse row

--- src/components/RoomDisplayCard.vue
+++ src/components/RoomDisplayCard.vue
@@ -6,21 +6,10 @@ defineProps({
 </script>
 
 <template>
-  <div
-    class="flex flex-col md:flex-row items-center md:items-center px-8 md:px-24"
-    :class="index % 2 === 1 ? 'md:flex-row-reverse' : ''"
-  >
-    
-    <img
-      :src="room.image"
-      :alt="room.name"
-      class="w-full md:w-1/2 rounded-md shadow-lg object-cover"
-    />
+  <div class="flex flex-col md:flex-row items-center md:items-center px-8 md:px-24" :class="index % 2 === 1 ? 'md:flex-row-reverse' : ''">
+    <img :src="room.image" :alt="room.name" class="w-full md:w-1/2 rounded-md shadow-lg object-cover"/>
 
-    <div
-      class="mt-6 md:mt-0 md:w-1/2"
-      :class="index % 2 === 1 ? 'md:text-left md:pr-10' : 'md:pl-10'"
-    >
+    <div class="mt-6 md:mt-0 md:w-1/2" :class="index % 2 === 1 ? 'md:text-left md:pr-10' : 'md:pl-10'">
       <h2 class="text-xl font-bold text-yellow-500">
         {{ room.name }}
       </h2>
