1b52ad17fef78e01f03e10df52bc1692730b3f2a
Sulthan Farizan Fawwaz <farizanfawwaz54@gmail.com>
2025-11-20 00:32:26 +0800

    done, maybe fix a little

---
+++ src/components/SnacksDisplayList.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { snacks } from '@/data/souvenir';
+import SnacksDisplayCard from './SnacksDisplayCard.vue';
+</script>
+
+<template>
+    <section class="bg-zinc-900 text-white pt-15 pb-20">
+        <h1 class="font-['Cinzel_Decorative'] text-center text-3xl md:text-5xl font-semibold text-yellow-500 ">Snacks</h1>
+        <p class="max-w-4xl mx-auto font-medium text-center text-gray-300 mt-5 leading-relaxed px-6">Nikmati Kelezatan Tradisi di genggaman anda</p>
+
+        <div class="mt-20 grid grid-cols-1 md:grid-cols-3 gap-12 px-6 lg:px-20">
+            <SnacksDisplayCard
+                v-for="s in snacks"
+                :key="s.id"
+                :snacks="s"
+        />
+        </div>
+    </section>
+</template>
