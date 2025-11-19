248aa55cc826da59577ba5dee60fe5681fc8a6ec
Sulthan Farizan Fawwaz <farizanfawwaz54@gmail.com>
2025-11-20 00:32:10 +0800

    update

--- src/pages/SouvenirsGiftsPage.vue
+++ src/pages/SouvenirsGiftsPage.vue
@@ -1,17 +1,11 @@
 <script setup lang="ts">
-
 import Navbar from "@/components/Navbar.vue";
+import SnacksDisplayList from "@/components/SnacksDisplayList.vue";
 </script>
 
 <template>
   <Navbar/>
-
-  <div class="p-4 text-center text-xl font-bold bg-green-100">
-    <h1>Snacks</h1>
-    <p>Nikmati Kelezatan Tradisi di genggaman anda.</p>
-    <h1>Souvenirs&Crafts</h1>
-    <p>Koleksi kami yang membawa sentuhan seni Timur Tengah</p>
-  </div>
+  <SnacksDisplayList/>
 </template>
 
 <style scoped>
