a39dbb5b6f6521332315d21e2e44de2a9fc1dac4
Sulthan Farizan Fawwaz <farizanfawwaz54@gmail.com>
2025-11-19 23:07:12 +0800

    change way to access path image

--- src/data/souvenir.js
+++ src/data/souvenir.js
@@ -3,17 +3,17 @@ export const snacks = [
     {
         id: 'snack1',
         name: 'Kurma',
-        image: '@/src/assets/images/souvenirs&gifts/kurma.jpg',
+        image: new URL('@/src/assets/images/souvenirs&gifts/kurma.jpg', import.meta.url).href
     },
     {
         id: 'snack2',
         name: 'Kismis',
-        image: '@/src/assets/images/souvenirs&gifts/kismis.jpeg'
+        image: new URL('@/src/assets/images/souvenirs&gifts/kismis.jpeg',import.meta.url).href
     },
     {
         id: 'snack3',
         name: 'Kacang Arab',
-        image: '@/src/assets/images/souvenirs&gifts/kacang arab.jpeg'
+        image: new URL('@/src/assets/images/souvenirs&gifts/kacang arab.jpeg', import.meta.url),href
     }
 ];
 
@@ -22,11 +22,11 @@ export const crafts = [
     {
         id: 'craft1',
         name: 'Tasbih',
-        image: '@/src/assets/images/souvenirs&gifts/tasbih.jpeg'
+        image: new URL('@/src/assets/images/souvenirs&gifts/tasbih.jpeg', import.meta.url),href
     },
     {
         id: 'craft2',
         name: 'Gantungan Kunci',
-        image: '@/src/assets/images/souvenirs&gifts/gantungan kunci.png'
+        image: new URL('@/src/assets/images/souvenirs&gifts/gantungan kunci.png', import.meta.url).href
     }
 ];
