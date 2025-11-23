fbf65694177b61f61c6803d72b297812cb35cafa
Sulthan Farizan Fawwaz <farizanfawwaz54@gmail.com>
2025-11-23 16:26:37 +0800

    Page done + responsive

--- src/pages/SouvenirsGiftsPage.vue
+++ src/pages/SouvenirsGiftsPage.vue
@@ -1,11 +1,15 @@
 <script setup lang="ts">
 import Navbar from "@/components/Navbar.vue";
 import SnacksDisplayList from "@/components/SnacksDisplayList.vue";
+import SouvenirsCraftsList from "@/components/SouvenirsCraftsList.vue";
+import Footer from "@/components/Footer.vue";
 </script>
 
 <template>
   <Navbar/>
   <SnacksDisplayList/>
+  <SouvenirsCraftsList/>
+  <Footer/>
 </template>
 
 <style scoped>
