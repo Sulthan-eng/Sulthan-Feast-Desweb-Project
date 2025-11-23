db5d6a7e01064abd7adde76384e8e3da19e55f87
Sulthan Farizan Fawwaz <farizanfawwaz54@gmail.com>
2025-11-23 16:26:28 +0800

    done components + responsive

---
+++ src/components/SouvenirsCraftsCard.vue
@@ -0,0 +1,20 @@
+<script setup>
+defineProps({
+    souvenirs: Object
+})
+</script>
+
+<template>
+    <div class="flex flex-col items-center bg-white w-80 h-auto rounded-lg shadow-md hover:shadow-xl transition-shadow duration-300 overflow-hidden">
+        <img :src="souvenirs.image" :alt="souvenirs.name" class="h-56 w-full object-cover"/>
+
+        <div class="p-4 w-full text-center">
+            <h3 class="mt-3 text-xl font-semibold text-black">
+                {{ souvenirs.name }}
+            </h3>
+            <p class="text-lg font-medium text-gray-800">
+                {{ souvenirs.price }}
+            </p>
+        </div>
+    </div>
+</template>
---
+++ src/components/SouvenirsCraftsList.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { souvenirs } from '@/data/souvenir';
+import SouvenirsCraftsCard from './SouvenirsCraftsCard.vue';
+</script>
+
+<template>
+    <section class="bg-zinc-900 text-white pt-15 pb-20">
+        <h1 class="font-['Cinzel_Decorative'] text-center text-3xl md:text-5xl font-semibold text-yellow-500">Souvenirs & Crafts</h1>
+        <p class="max-w-4xl mx-auto font-medium text-center text-gray-300 mt-5 leading-relaxed px-6">Koleksi kami yang membawa sentuhan seni Timur Tengah</p>
+
+        <div class="mt-10 md:mt-15 grid grid-cols-1 md:grid-cols-2 justify-items-center gap-16 md:gap-8 px-6 lg:px-20">
+            <SouvenirsCraftsCard
+                v-for="sou in souvenirs"
+                :key="sou.id"   
+                :souvenirs="sou" 
+            />
+        </div>
+    </section>
+</template>
