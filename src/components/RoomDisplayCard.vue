196107c613bc2aab2a42570198c8b941e85b2b57
Sulthan Farizan Fawwaz <farizanfawwaz54@gmail.com>
2025-11-23 14:24:35 +0800

    fix and responsive room

--- src/components/RoomDisplayCard.vue
+++ src/components/RoomDisplayCard.vue
@@ -6,21 +6,11 @@ defineProps({
 </script>
 
 <template>
-  <div
-    class="flex flex-row items-center px-4 md:px-24"
-    :class="index % 2 === 1 ? 'flex-row-reverse' : ''"
-  >
+  <div class="flex flex-row items-center px-4 md:px-24" :class="index % 2 === 1 ? 'flex-row-reverse' : ''">
     
-    <img
-      :src="room.image"
-      :alt="room.name"
-      class="w-1/2 rounded-md shadow-lg object-cover"
-    />
+    <img :src="room.image" :alt="room.name" class="w-1/2 rounded-md shadow-lg object-cover"/>
 
-    <div
-      class="w-1/2"
-      :class="index % 2 === 1 ? 'text-left pr-4 md:pr-10' : 'pl-4 md:pl-10'"
-    >
+    <div class="w-1/2" :class="index % 2 === 1 ? 'text-left pr-4 md:pr-10' : 'pl-4 md:pl-10'">
       <h2 class="text-sm md:text-xl font-bold text-yellow-500">
         {{ room.name }}
       </h2>
