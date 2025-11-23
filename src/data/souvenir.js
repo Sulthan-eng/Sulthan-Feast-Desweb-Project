51243ca3fd36e3a391195eee242abf0e683dcd21
Sulthan Farizan Fawwaz <farizanfawwaz54@gmail.com>
2025-11-23 15:02:25 +0800

    fix the path and id

--- src/data/souvenir.js
+++ src/data/souvenir.js
@@ -3,30 +3,35 @@ export const snacks = [
     {
         id: 'snack1',
         name: 'Kurma',
+        price: 'RP 29.000 (500 gr)',
         image: new URL('@/assets/images/souvenirs-gifts/kurma.jpg', import.meta.url).href
     },
     {
         id: 'snack2',
         name: 'Kismis',
+        price: 'RP 26.000 (500 gr)',
         image: new URL('@/assets/images/souvenirs-gifts/kismis.jpg', import.meta.url).href
     },
     {
         id: 'snack3',
         name: 'Kacang Arab',
+        price: 'RP 21.000 (500 gr)',
         image: new URL('@/assets/images/souvenirs-gifts/kacang_arab.jpeg', import.meta.url).href
     }
 ];
 
-//SOUVENIRS & CRAFTS
-export const crafts = [
+//SOUVENIRS
+export const souvenirs = [
     {
-        id: 'craft1',
+        id: 'souvenir1',
         name: 'Tasbih',
+        price: 'RP 12.000',
         image: new URL('@/assets/images/souvenirs-gifts/tasbih.jpeg', import.meta.url).href
     },
     {
-        id: 'craft2',
+        id: 'souvenir2',
         name: 'Gantungan Kunci',
+        price: 'RP 10.000',
         image: new URL('@/assets/images/souvenirs-gifts/gantungan_kunci.png', import.meta.url).href
     }
 ];
