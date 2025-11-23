5848a456951a223fbd1a5250551f1a4a9d4f0c77
Sulthan Farizan Fawwaz <farizanfawwaz54@gmail.com>
2025-11-23 14:10:27 +0800

    fix layouting

--- src/components/RoomDisplayCard.vue
+++ src/components/RoomDisplayCard.vue
@@ -6,15 +6,26 @@ defineProps({
 </script>
 
 <template>
-  <div class="flex flex-col md:flex-row items-center md:items-center px-8 md:px-24" :class="index % 2 === 1 ? 'md:flex-row-reverse' : ''">
-    <img :src="room.image" :alt="room.name" class="w-full md:w-1/2 rounded-md shadow-lg object-cover"/>
+  <div
+    class="flex flex-row items-center px-4 md:px-24"
+    :class="index % 2 === 1 ? 'flex-row-reverse' : ''"
+  >
+    
+    <img
+      :src="room.image"
+      :alt="room.name"
+      class="w-1/2 rounded-md shadow-lg object-cover"
+    />
 
-    <div class="mt-6 md:mt-0 md:w-1/2" :class="index % 2 === 1 ? 'md:text-left md:pr-10' : 'md:pl-10'">
-      <h2 class="text-xl font-bold text-yellow-500">
+    <div
+      class="w-1/2"
+      :class="index % 2 === 1 ? 'text-left pr-4 md:pr-10' : 'pl-4 md:pl-10'"
+    >
+      <h2 class="text-sm md:text-xl font-bold text-yellow-500">
         {{ room.name }}
       </h2>
 
-      <p class="text-gray-300 mt-3 font-medium leading-relaxed">
+      <p class="text-xs md:text-base text-gray-300 mt-2 md:mt-3 leading-relaxed">
         {{ room.description }}
       </p>
     </div>
