ed67ea29fc082e4271e0eed03314f7fa40bbe767
Sulthan Farizan Fawwaz <farizanfawwaz54@gmail.com>
2025-11-23 19:27:27 +0800

    responsive done

--- src/components/SnacksDisplayList.vue
+++ src/components/SnacksDisplayList.vue
@@ -8,7 +8,7 @@ import SnacksDisplayCard from './SnacksDisplayCard.vue';
         <h1 class="font-['Cinzel_Decorative'] text-center text-3xl md:text-5xl font-semibold text-yellow-500 ">Snacks</h1>
         <p class="max-w-4xl mx-auto font-medium text-center text-gray-300 mt-5 leading-relaxed px-6">Nikmati Kelezatan Tradisi di genggaman anda</p>
 
-        <div class="mt-10 md:mt-15 grid grid-cols-1 md:grid-cols-3 justify-items-center gap-16 md:gap-8 px-6 lg:px-20">
+        <div class="mt-10 md:mt-15 grid grid-cols-1 lg:grid-cols-3 justify-items-center gap-16 md:gap-8 px-6 lg:px-20">
             <SnacksDisplayCard
                 v-for="s in snacks"
                 :key="s.id"
--- src/components/SouvenirsCraftsList.vue
+++ src/components/SouvenirsCraftsList.vue
@@ -8,7 +8,7 @@ import SouvenirsCraftsCard from './SouvenirsCraftsCard.vue';
         <h1 class="font-['Cinzel_Decorative'] text-center text-3xl md:text-5xl font-semibold text-yellow-500">Souvenirs & Crafts</h1>
         <p class="max-w-4xl mx-auto font-medium text-center text-gray-300 mt-5 leading-relaxed px-6">Koleksi kami yang membawa sentuhan seni Timur Tengah</p>
 
-        <div class="mt-10 md:mt-15 grid grid-cols-1 md:grid-cols-2 justify-items-center gap-16 md:gap-8 px-6 lg:px-20">
+        <div class="mt-10 md:mt-15 grid grid-cols-1 lg:grid-cols-2 justify-items-center gap-16 md:gap-8 px-6 lg:px-20">
             <SouvenirsCraftsCard
                 v-for="sou in souvenirs"
                 :key="sou.id"   
