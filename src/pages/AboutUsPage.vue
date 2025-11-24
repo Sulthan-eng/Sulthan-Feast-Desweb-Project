fd08e786ab22a6078abe3eb4b9941db7043ccf4d
Sulthan Farizan Fawwaz <farizanfawwaz54@gmail.com>
2025-11-24 09:43:46 +0800

    maps antara iya dan tidak utk di add

--- src/pages/AboutUsPage.vue
+++ src/pages/AboutUsPage.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
 import AboutUs from "@/components/AboutUs.vue";
 import Navbar from "@/components/Navbar.vue";
+import Footer from "@/components/Footer.vue";
 </script>
 
 <template>
   <Navbar/>
   <AboutUs/> 
+  <Footer/>
 </template>
 
 <style scoped>
