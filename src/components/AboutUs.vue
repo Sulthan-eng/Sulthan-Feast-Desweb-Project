df714f78b1439b16f04b02fddda4cf766eb83c5d
Sulthan Farizan Fawwaz <farizanfawwaz54@gmail.com>
2025-11-24 09:39:36 +0800

    udah oke sih, tinggal fix kan dikit

---
+++ src/components/AboutUs.vue
@@ -0,0 +1,21 @@
+<script setup>
+</script>
+
+<template>
+    <div class="text-white bg-zinc-900 pt-15 pb-20">
+        <h1 class="font-['Cinzel_Decorative'] text-3xl text-center md:text-5xl font-semibold text-yellow-500">Our Story</h1>
+
+        <div class="max-w-6xl mx-auto flex flex-col md:flex-row items-center p-6 md:p-8 gap-10 md:gap-20">
+            <div class="w-full md:w-1/2 flex justify-center md:justify-end">
+                <img src="@/assets/images/menu/maincourse-sidedish/kebuli_sapi.png" alt="Sulthan Feast Story" class="w-full max-w-lg rounded-lg shadow-lg object-cover">
+            </div>
+
+            <div class="w-full md:w-1/2 text-center md:text-left">
+                <p class="text-gray-200 leading-relaxed text-base md:text-lg text-justify">
+                    Selamat datang di Sulthan Feast. Kisah kami adalah tentang otentisitas—sebuah resep warisan dari Hadramaut, Yaman, yang kami hidupkan kembali di Balikpapan. Kami percaya rasa sejati lahir dari bahan baku terbaik. Karena itu, kami hanya menggunakan racikan bumbu asli dari Timur Tengah yang menghasilkan aroma masakan yang khas dan kuat. Kami tidak hanya menyajikan hidangan, tetapi juga sebuah suasana lengkap dengan interior dan musik yang akan membawa Anda merasakan langsung suasana Timur Tengah.
+                </p>
+            </div>
+        </div>
+
+    </div>
+</template>
