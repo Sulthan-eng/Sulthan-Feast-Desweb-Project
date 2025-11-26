ec2d4e7e92e48b73fcb6f14810eee15109fcdb62
Sulthan Farizan Fawwaz <farizanfawwaz54@gmail.com>
2025-11-27 00:02:54 +0800

    change postition style.css

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import './style.css'
+import '@/assets/style.css'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { faInstagram, faFacebook } from '@fortawesome/free-brands-svg-icons'
