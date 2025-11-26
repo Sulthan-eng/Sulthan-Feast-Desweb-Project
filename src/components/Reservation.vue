eac8f3f47901f8c1958209e8cbd8e32ca110f04b
Sulthan Farizan Fawwaz <farizanfawwaz54@gmail.com>
2025-11-26 21:50:52 +0800

    baikin layout

--- src/components/Reservation.vue
+++ src/components/Reservation.vue
@@ -155,7 +155,7 @@ const availableTimes = [
               <label for="time" class="block text-sm/6 font-medium text-white"
                 >Jam Reservasi*</label
               >
-              <div class="mt-2">
+              <div class="mt-2 grid grid-cols-1">
                 <select
                   id="time"
                   name="time"
@@ -183,6 +183,7 @@ const availableTimes = [
                   v-model="formData.guests"
                   required
                   class="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-yellow-500 sm:text-sm/6"
+                  placeholder="1"
                 />
               </div>
             </div>
