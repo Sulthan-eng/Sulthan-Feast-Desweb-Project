c23654ac0581e5ea474a89e081a1fbaf13326c10
Sulthan Farizan Fawwaz <farizanfawwaz54@gmail.com>
2025-11-30 21:52:27 +0800

    finish for a while

--- src/components/MenuTab.vue
+++ src/components/MenuTab.vue
@@ -1,25 +1,88 @@
 <script setup>
-import { Update } from 'vite';
+import { ref, computed } from 'vue'
+import MenuItemCard from './MenuItemCard.vue'
 
-const props = defineProps({
-    activeTab: String,
-})
+import { appetizerMenu } from '@/data/menu'
+import { mainCourseMenu } from '@/data/menu'
+import { sideDishMenu } from '@/data/menu'
+import { beverageMenu } from '@/data/menu'
+import { dessertMenu } from '@/data/menu'
 
-const emit = defineEmits(["update:activeTab"])
+
+const activeTab = ref('appetizer')
+
+
 const tabs = [
-   {value: "Appertizer", id: "appertizer"},
-   {value: "Main Couser", id: "maincourse"},
+   {value: "Appetizer", id: "appetizer"},
+   {value: "Main Couse", id: "maincourse"},
    {value: "Side Dish", id: "sidedish"},
    {value: "Beverage", id: "beverage"},
    {value: "Dessert", id: "dessert"},
 ]
+
+// logika pemilih data
+const currentMenuData = computed(() => {
+    switch (activeTab.value) {
+        case 'appetizer': return appetizerMenu;
+        case 'maincourse': return mainCourseMenu;
+        case 'sidedish': return sideDishMenu;
+        case 'beverage': return beverageMenu;
+        case 'dessert': return dessertMenu;
+        default : return[]; 
+    } 
+})
+
+//logika cek nested
+const isNested = computed (() => {
+    const data = currentMenuData.value;
+    return data && data.length > 0 && data[0].items;
+})
 </script>
 
 <template>
-    <div class="flex gap-3 justify-center mt-6">
-        <button v-for="tab in tabs" :key="tab.value" @click="$emit('update:activeTab', tab.value)"
-            class="px-4 py-2 rounded-lg border" :class="activeTab === tab.id ? 'bg-yellow-500 text-gray-200 font-semibold' : 'bg-black text-gray-200 font-semibold'">
-            {{ tab.value }}
-        </button>
+    <div class="bg-zinc-900 container mx-auto mb-20">
+        <div class="flex gap-3 justify-center mt- mb-10 flex-wrap">
+            <button v-for="tab in tabs" :key="tab.value" @click="activeTab = tab.id"
+                class="px-8 py-4 mx-2 rounded-xl border transition-all duration-150 text-sm md:text-base cursor-pointer font-semibold tracking-wide" 
+                :class="activeTab === tab.id ? 'bg-amber-500 text-gray-50 border-amber-500 shadow-lg shadow-amber-500/35' : 'bg-black text-gray-200 border-amber-400 hover:border-amber-600 hover:text-amber-500'">
+                {{ tab.value }}
+            </button>
+        </div>
+
+        <div class="min-h-[300px]"> 
+            <div v-if="currentMenuData.length === 0" class="text-center text-gray-500 py-10 italic">
+                Menu tidak tersedia.
+            </div>
+
+            <div v-else-if="isNested" class="flex flex-col gap-16">
+                <div v-for="(group, index) in currentMenuData" :key="index">
+                    
+                    <div class="mb-6 font-serif font-bold text-yellow-500">
+                        <h2 class="text-2xl">
+                            {{ group.groupName }}
+                        </h2>
+                        <p v-if="group.groupDesc" class="text-gray-300 text-md mt-1.5 max-w-2xl" >
+                            {{ group.groupDesc }}
+                        </p>
+                    </div>
+
+                    <div class="grid grid-cols-1 md:grid-cols-2  gap-x-6 gap-y-8">
+                        <MenuItemCard
+                            v-for="item in group.items"
+                            :key="item.id"
+                            :item="item"  
+                        />
+                    </div>
+                </div>
+            </div>
+
+            <div v-else class="grid grid-cols-1 md:grid-cols-2  gap-x-6 gap-y-8">
+                <MenuItemCard 
+                    v-for="item in currentMenuData" 
+                    :key="item.id" 
+                    :item="item" 
+                />
+            </div>
+        </div>
     </div>
 </template>
