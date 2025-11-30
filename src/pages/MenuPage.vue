ad630be4185e11223e48ccda3b18073c66b2f680
Sulthan Farizan Fawwaz <farizanfawwaz54@gmail.com>
2025-11-30 16:20:43 +0800

    add MenuTab component

--- src/pages/MenuPage.vue
+++ src/pages/MenuPage.vue
@@ -1,11 +1,13 @@
-<script setup lang="ts">
-
+<script setup lang="ts"> 
+import MenuHeader from "@/components/MenuHeader.vue";
+import MenuTab from "@/components/MenuTab.vue";
 import Navbar from "@/components/Navbar.vue";
 </script>
 
 <template>
   <Navbar/>
-
+  <MenuHeader/>
+  <MenuTab/>
 </template>
 
 <style scoped>
