77993e2b55df0822fdd192a07358875dd750a374
Sulthan Farizan Fawwaz <farizanfawwaz54@gmail.com>
2025-11-30 21:51:50 +0800

    add  Footer component

--- src/pages/MenuPage.vue
+++ src/pages/MenuPage.vue
@@ -2,12 +2,14 @@
 import MenuHeader from "@/components/MenuHeader.vue";
 import MenuTab from "@/components/MenuTab.vue";
 import Navbar from "@/components/Navbar.vue";
+import Footer from "@/components/Footer.vue";
 </script>
 
 <template>
   <Navbar/>
   <MenuHeader/>
   <MenuTab/>
+  <Footer/>
 </template>
 
 <style scoped>
