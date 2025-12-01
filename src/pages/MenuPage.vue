38094f1438fb7e35fc70991d39c35d05365946d7
Sulthan Farizan Fawwaz <farizanfawwaz54@gmail.com>
2025-12-02 00:19:19 +0800

    nambah state search dan fungsi

--- src/pages/MenuPage.vue
+++ src/pages/MenuPage.vue
@@ -1,14 +1,21 @@
 <script setup lang="ts"> 
+import { ref } from "vue";
 import MenuHeader from "@/components/MenuHeader.vue";
 import MenuTab from "@/components/MenuTab.vue";
 import Navbar from "@/components/Navbar.vue";
 import Footer from "@/components/Footer.vue";
+
+const searchKeyword = ref('')
+
+const handleSearch = (keyword) => {
+  searchKeyword.value = keyword;
+}
 </script>
 
 <template>
   <Navbar/>
-  <MenuHeader/>
-  <MenuTab/>
+  <MenuHeader @search="handleSearch"/>
+  <MenuTab :searchQuery="searchKeyword"/>
   <Footer/>
 </template>
 
