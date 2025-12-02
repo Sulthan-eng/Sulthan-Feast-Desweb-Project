24b679e30f87b1c810d4b29c228e6456865be9b7
Sulthan Farizan Fawwaz <farizanfawwaz54@gmail.com>
2025-12-03 00:46:09 +0800

    add action when move to other page

--- src/router/index.js
+++ src/router/index.js
@@ -40,10 +40,14 @@ const routes = [
 ]
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes,  //route udah didefinisikan
+  routes,  
+
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition
+    }
+    return { top: 0 }
+  }
 })
 
 export default router
-
-
-
