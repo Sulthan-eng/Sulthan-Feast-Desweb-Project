141cfaecfefe971ac7a6332cef814cac11030775
Sulthan Farizan Fawwaz <farizanfawwaz54@gmail.com>
2025-12-03 00:59:21 +0800

    change length of search coloumn

--- src/components/MenuHeader.vue
+++ src/components/MenuHeader.vue
@@ -14,7 +14,7 @@ const handleInput = (e) => {
         </p>
 
         <div class="mt-8 w-full flex justify-center">
-            <div class="relative w-full max-w-md">
+            <div class="relative w-full max-w-2xl">
                 <div class="absolute inset-y-0 left-0 flex items-center pl-4 pointer-events-none">
                     <svg class="w-5 h-5 text-gray-600" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
