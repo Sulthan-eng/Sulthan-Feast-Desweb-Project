d9d2f0734a72c33d6bede142693136651ca97883
Sulthan Farizan Fawwaz <farizanfawwaz54@gmail.com>
2025-12-02 23:59:56 +0800

    delete unuse code cz of template tailwind

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -6,9 +6,6 @@ import heroImg from "@/assets/images/heroSection/hero_foto.jpg";
 <div :style="{backgroundImage: `url(${heroImg})`}"  class="relative bg-cover bg-center min-h-screen">
   <div class="absolute inset-0 bg-[rgba(0,0,0,0.5)]"></div>    
   <div class="relative isolate px-6 pt-14 lg:px-8">
-    <div aria-hidden="true" class="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80">
-      <div style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)" class="relative left-[calc(50%-11rem)] aspect-1155/678 w-144.5 -translate-x-1/2 rotate-30 bg-linear-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-288.75"></div>
-    </div>
     <div class="mx-auto max-w-2xl py-24 sm:py-32 lg:py-48">
       <div class="text-center">
         <h1 class="text-4xl font-semibold tracking-normal text-balance text-white sm:text-5xl lg:text-7xl">Welcome to Sulthan Feast!</h1>
