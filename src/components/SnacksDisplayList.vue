fc19f7fc812f3ddd3ef4ae9be1b01dfd5f1bcf83
Sulthan Farizan Fawwaz <farizanfawwaz54@gmail.com>
2025-12-03 22:26:38 +0800

    fix the structure (got switched)

--- src/components/SnacksDisplayList.vue
+++ src/components/SnacksDisplayList.vue
@@ -17,7 +17,7 @@ import SnacksDisplayCard from "./SnacksDisplayCard.vue";
     </p>
 
     <div
-      class="mt-10 grid grid-cols-1 justify-items-center gap-16 px-6 md:mt-15 md:gap-8 lg:grid-cols-3 lg:px-20"
+      class="grid grid-cols-1 lg:grid-cols-3 justify-items-center gap-16 md:gap-8 mt-10 md:mt-15 px-6 lg:px-20"
     >
       <SnacksDisplayCard v-for="s in snacks" :key="s.id" :snacks="s" />
     </div>
